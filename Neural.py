0af7d6b8c57868d02e8c80780d9fc41e0b2e33de
mke26 <mke26@case.edu>
2024-10-10 17:56:41 -0400

    fixing Neural.py

--- Neural.py
+++ Neural.py
@@ -20,7 +20,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Normalize data
 X_train = X_train / 255.0
 X_test = X_test / 255.0
-num_classes = 10
+num_classes = 100
 y_train_encoded = to_categorical(y_train, num_classes)
 y_test_encoded = to_categorical(y_test, num_classes)
 
@@ -35,11 +35,11 @@ model = tf.keras.Sequential([
 ])
 
 # Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy')
 
 # Train Neural Network Classifier
-losses = model.fit(X_train, y_train_encoded, validation_split=0.1, batch_size=256, epochs=50, verbose=0)
-print(losses)
+losses = model.fit(X_train, y_train_encoded, validation_data=(X_test, y_test_encoded), batch_size=256, epochs=50)
+print(losses.history)
 
 # Predict the response for test dataset
 y_pred_encoded = model.predict(X_test)
@@ -51,7 +51,6 @@ print("Accuracy:", accuracy_score(y_test, y_pred))
 print("Precision:", precision_score(y_test, y_pred, average='weighted'))
 print("Recall:", recall_score(y_test, y_pred, average='weighted'))
 print("F1 Score:", f1_score(y_test, y_pred, average='weighted'))
-print("Loss:", losses.history['loss'][-1])
 
 # Full training set for final predictions 
 X_full = X / 255.0
